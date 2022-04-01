5309ebc5a71293f5166992c9154c7d8bb0afff90
Roger <rsd.santo@outlook.com>
2022-04-01 18:37:31 -0300

    Iniciando LAYOUT do dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,13 +1,58 @@
-import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 import { Container } from './styled';
 
+// icons 
+import { MdAccountCircle } from "react-icons/md";
+
 const Dashboard = ()=>{
-    const name = useSelector(state=>state.user.name);
+
 
     return (
         <Container>
-            <h2>Painel Administrativo</h2>
-            <p>Ola {name}</p>
+            <div className="container-itens">
+                <div className="header">
+                    <p><MdAccountCircle />Ola, Bem vindo</p>
+                    <Link to='/'>Sair</Link>
+                </div>
+
+                <div className="description">
+                    <Link to='/' className="balance">
+                        <p>Saldo Atual</p>
+                        <span>R$ 2.557,45</span>
+                    </Link>
+
+                    <Link to='/' className="credit_card">
+                        <p>Cartão de Crédito</p>
+                        <span>Limite Disponivel</span>
+                        <span>R$ 3.115,09</span>
+                    </Link>
+
+                    <Link to='/' className="loan">
+                        <p>Emprestimo</p>
+                        <span>
+                            Precisando de dinheiro?<br />
+                            podemos ajudar.
+                        </span>
+                    </Link>
+
+                    <Link to='/' className="pix">
+                        <p>Pix</p>
+                        <span>Deseja transferir ou pagar um boleto</span>
+                    </Link>
+
+                    <Link to='/' className="transfer">
+                        <p>Transferencia</p>
+                        <span>Deseja fazer uma transferencia de valores</span>
+                    </Link>
+
+                    <Link to='/' className="donation">
+                        <p>Doar</p>
+                        <span>Ajude uma instituição</span>
+                    </Link>
+                </div>
+
+            </div>
         </Container>
     )
 };
--- src/pages/Dashboard/styled.js
+++ src/pages/Dashboard/styled.js
@@ -6,8 +6,77 @@ export const Container = styled.div`
     background-color: #73C2FF;
     color: #fff;
     display:flex;
-    justify-content: center;
     align-items: center;
     flex-direction: column;
     font-size:16px;
+
+    .container-itens{
+        width: 1200px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+
+        .header{
+            width: 80%;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-top:50px;
+
+            p{
+                font-size:1.2em;
+            }
+
+            svg{
+                font-size:1.5em;
+                position: absolute;
+                margin-left:-30px;
+                margin-top:-5px;
+            }
+
+            a{
+                color:#fff;
+                font-size:1.2em;
+            }
+    
+        }
+
+        .description{
+            width: 100%;
+            display: flex;
+            justify-content:space-around;
+            flex-wrap: wrap;
+            margin-top:100px;
+
+
+            .balance, .credit_card, .loan, .pix, .transfer, .donation{
+                width:calc(80% / 3);
+                min-height: 200px;
+                display: flex;
+                flex-direction: column;
+                padding:7px;
+                margin: 20px 0;
+                border-radius:7px;
+                background-color: #68AFE6;
+                text-align:center;
+                text-decoration:none;
+                color: #fff;
+
+
+                p{
+                    margin-top:20px;
+                    font-size:1.3em;
+                }
+
+                span{
+                    margin-top:20px;
+                }
+
+                &:hover{
+                    background-color: #53a6e3;
+                }
+            }
+        }
+    }
 `;
